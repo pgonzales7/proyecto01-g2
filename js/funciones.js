09c9381ed17dc9a5ec520ce3834e47a421a5a521
pgonzales7 <118759321+pgonzales7@users.noreply.github.com>
2022-11-21 12:26:41 -0500

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,19 @@
+/*
+Generales
+-> Todo linea de codigo o instruccion (bloque) termina ; (punto y coma)
+-> JS tiene funciones integradas -> alert, prompt, confirm
+-> JS tiene librerias integradas -> Math (Conjunto de funciones matematicas)
+-> JS Usa concepto de programacion Orientada a Objetos (Atributos, Métodos)
+
+
+
+*/
+// Primer ejemplo de JS
+alert("Bienvenido");
+// Definiendo o declarando la variable
+var nombre="Carlota";
+alert(nombre);
+// Cambiando el valor a la variable
+nombre = "Juan";
+alert(nombre);
+alert("Tu nombre es: " + nombre);
